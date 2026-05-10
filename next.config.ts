b3bdbaf8f6859214bee51f52212d24c15fac211c
suraj das <itzzsurajzz@gmail.com>
2026-05-10 13:08:01 +0530

    upload next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: 'standalone',
+  // other config options can stay here
+};
+  /* config options here */
+};
+
+export default nextConfig;
